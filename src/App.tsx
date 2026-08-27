d7d31f1b7b0784e425e03d4c15cbc1ec5f650aef
KatraSterner <Rollie406@gmail.com>
2026-08-27 14:17:34 -0500

    navigation animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
-import Navigation from "./global-components/Navigation.tsx";
+import Navigation from "./components/Navigation.tsx";
 import Contact from "./pages/Contact.tsx";
 import Experience from "./pages/Experience.tsx";
 import Projects from "./pages/Projects.tsx";
@@ -12,7 +12,13 @@ function App() {
     <div className={'bg-blank text-blank min-h-screen p-5'}>
       {/* maybe move to separate component later */}
       <BrowserRouter>
-        <Navigation />
+        {/*
+        <p className={'bg-green-400 sm:bg-blue-400 md:bg-red-400 lg:bg-pink-400 xl:bg-purple-400 2xl:bg-amber-400'}>example</p>
+        */}
+        <div className={'flex justify-center m-1 mt-8'}>
+          <Navigation />
+        </div>
+
 
         <Routes>
           <Route path="/" element={<Home />} />
