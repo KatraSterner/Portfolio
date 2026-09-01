26726136558be0169e381a2075dafeedbf5bef5f
KatraSterner <Rollie406@gmail.com>
2026-08-31 19:59:33 -0500

    link added

--- src/Home/HomeHeader.tsx
+++ src/Home/HomeHeader.tsx
@@ -10,11 +10,12 @@ const HomeHeader = () => {
                     <p className={"text-[clamp(4rem,6vw,6.5rem)] italic font-bold leading-tight"}>S t e r n e r</p>
                 </div>
                 <p className={"text-[clamp(2rem,2vw,3rem)] font-bold"}>Software Engineer</p>
-                <motion.button
+                <motion.a
+                    href={'/about'}
                     className={"self-start text-[clamp(1.25rem,1.5vw,1.75rem)] font-bold bg-primary-2 rounded-2xl p-3 px-12 mt-6 shadow-md hover:shadow-lg"}
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.95 }}
-                >Get to Know Me</motion.button>
+                >Get to Know Me</motion.a>
             </div>
 
             <div className={'absolute right-0 top-0 h-full w-[45%] rounded-2xl overflow-hidden aspect-[3/4]'}>
